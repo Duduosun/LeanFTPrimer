c10bb3cfb2ca81f351e3acd48c377e3356e05298
Duduosun <tola.fasoyiro@hotmail.co.uk>
2015-10-16 12:16:42 +0100

    FirstCommit_161015

---
+++ LeanFTPrimer/LeanFtTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HP.LFT.SDK.Web;
+
+namespace LeanFTPrimer
+{
+    [TestClass]
+    public class LeanFtTest : UnitTestClassBase<LeanFtTest>
+    {
+        //IBrowser browser = BrowserFactory.Launch(BrowserType.InternetExplorer);
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            GlobalSetup(context);
+            
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        public void TestMethod1()
+        {
+            IBrowser browser = BrowserFactory.Launch(BrowserType.InternetExplorer);
+            browser.Navigate("www.salmon.com");
+            browser.Sync();
+            browser.Close();
+            //browser.CloseAllTabs();
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void TestMethod2()
+        {
+            IBrowser browser = BrowserFactory.Launch(BrowserType.InternetExplorer);
+            browser.Navigate("www.google.com");
+            browser.Sync();           
+        }
+            
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            GlobalTearDown();
+        }
+    }
+}
